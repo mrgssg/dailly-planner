b8e6b24a72b751929e7ab9007f6635423943faff
Mark Green <mrgssg@gmail.com>
2023-01-18 13:57:12 -0500

    added textarea and cleaned upcolumn align a little

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 // Using moment.js to populate the current day
 var today = moment();
 $("#currentDay").text(today.format('dddd, MMMM Do YYYY'))
+var time = moment()
 
+
+
+
+
+// save button feature 
+$(".saveBtn").on("click", function () {
+    localStorage.setItem($(".hour"), text);
+})
