2b5b00c233f46a58238ca3db53b139d0edea4d3e
Mark Green <mrgssg@gmail.com>
2023-01-18 19:24:00 -0500

    fixed time block coloring and local storage

--- script.js
+++ script.js
@@ -2,12 +2,39 @@
 var today = moment();
 $("#currentDay").text(today.format('dddd, MMMM Do YYYY'))
 
-
+$(".description").each(function() {
+    var currentTime = moment().hour(); 
+    var rowHour = $(this).parentsUntil().attr("id")
+    // console.log(rowHour)
+    if (rowHour < currentTime) {
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+        $(this).addClass("past");
+    }
+    else if (rowHour === currentTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    }
+})
 
 
 
 // save button feature 
 $(".saveBtn").on("click", function () {
-    var text = $(this).append("discription").val();
-    localStorage.setItem($(".hour"), text);
+   var description = $(this).siblings(".description").val()
+   var time = $(this).parent().attr("id")
+//    console.log(description)
+    localStorage.setItem(time, description);
 })
+
+
+for (var i = 9; i < 16; i++) {
+    $(`#${i} .description`).val(localStorage.getItem(`${i}`))
+}
+
